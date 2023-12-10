3cc5e70f7363bc4611697a01174f292c60af248b
Neto test <s.e.lebedeva74@gmail.com>
2023-12-10 15:33:50 +0500

    Parameterized test

--- src/test/java/ru/netology/javaqa/HW_CYCLES/services/WorkCycleOfFreelancerTest.java
+++ src/test/java/ru/netology/javaqa/HW_CYCLES/services/WorkCycleOfFreelancerTest.java
@@ -2,34 +2,22 @@ package ru.netology.javaqa.HW_CYCLES.services;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class WorkCycleOfFreelancerTest {
 
-    @Test
-    void WorkCycleOfFreelancerTest1() {
+    @ParameterizedTest
+    @CsvSource({
+            "10000, 3000, 20000, 3",
+            "100000, 60000, 150000, 2"
+    })
+    void WorkCycleOfFreelancerTest(int income, int expenses, int threshold, int expected) {
         WorkCycleOfFreelancer service = new WorkCycleOfFreelancer();
-
-        int income = 10_000;
-        int expenses = 3_000;
-        int threshold = 20_000;
-        int expected = 3;
 
         int actual = service.MonthsOfRest(income, expenses, threshold);
 
         Assertions.assertEquals(expected, actual);
     }
 
-    @Test
-    void WorkCycleOfFreelancerTest2() {
-        WorkCycleOfFreelancer service = new WorkCycleOfFreelancer();
-
-        int income = 100_000;
-        int expenses = 60_000;
-        int threshold = 150_000;
-        int expected = 2;
-
-        int actual = service.MonthsOfRest(income, expenses, threshold);
-
-        Assertions.assertEquals(expected, actual);
-    }
 }
